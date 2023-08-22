f4d8be02441665af4c5807b709624a327c29e5fe
Kristina <Bolshakovakristy@gmail.com>
2023-08-22 14:23:09 +0300

    HW2 Task1 Done

--- src/HW2/Task1.java
+++ src/HW2/Task1.java
@@ -13,12 +13,15 @@
  * Массив координат пути или сообщение об ошибке.
  */
 
+//[0, 0, 1, 0, 0]
+//[0, 0, 0, 0, 1]
+//[0, 1, 1, 0, 0]
+//[0, 0, 0, 1, 0]
+//[0, 1, 0, 0, 0]
+
 package HW2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -26,6 +29,13 @@ public class Task1 {
         ArrayList<Integer> arrCoor = inOut(input);
         int[][] array = randMas(arrCoor);
         printMas(array);
+        ArrayList<int[]> mazePath = moveInMaze(array, arrCoor);
+
+        if(mazePath != null){
+            for (int i = 0; i < mazePath.size(); i++) {
+                System.out.println(Arrays.toString(mazePath.get(i)));
+            }
+        }
     }
 
     /**
@@ -47,9 +57,9 @@ public class Task1 {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if ((i == ix) && (j == iy)) {
-                    mas[i][j] = 3;
+                    mas[i][j] = 0;
                 } else if ((i == ox) && (j == oy)) {
-                    mas[i][j] = 3;
+                    mas[i][j] = 0;
                 } else {
                     mas[i][j] = rand.nextInt(2);
                 }
@@ -161,7 +171,80 @@ public class Task1 {
         return arrList;
     }
 
-    public static void moveInMaze(int[][] masArray){
+    public static ArrayList<int[]> moveInMaze(int[][] maze, ArrayList<Integer> arr){
+        int m = arr.get(0);
+        int n = arr.get(1);
+        int ix = arr.get(2);
+        int iy = arr.get(3);
+        int ox = arr.get(4);
+        int oy = arr.get(5);
 
+//        int m = maze.length;
+//        int n = maze[0].length;
+
+        // Проверяем, что начальная и конечная точки находятся в пределах лабиринта
+        if (ix < 0 || ix >= m || iy < 0 || iy >= n || ox < 0 || ox >= m || oy < 0 || oy >= n) {
+            System.out.println("Начальная или конечная точка находятся за пределами лабиринта.");
+            return null;
+        }
+
+        // Проверяем, что начальная и конечная точки не являются стенами
+        if (maze[ix][iy] == 1 || maze[ox][oy] == 1) {
+            System.out.println("Начальная или конечная точка находятся на стене.");
+            return null;
+        }
+
+        // Массив для хранения пути
+        ArrayList<int[]> path = new ArrayList<>();
+
+        // Массив, используемый для отслеживания уже посещенных точек
+        boolean[][] visited = new boolean[m][n];
+
+        // Создаем очередь для поиска в ширину
+        Queue<int[]> queue = new LinkedList<>();
+
+        // Добавляем начальную точку в очередь и отмечаем ее как посещенную
+        queue.offer(new int[]{ix, iy});
+        visited[ix][iy] = true;
+
+        // Массив для хранения направлений движения: вверх, вниз, влево, вправо
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        // Пока очередь не пуста, продолжаем поиск
+        while (!queue.isEmpty()) {
+            // Извлекаем текущую точку из очереди
+            int[] curr = queue.poll();
+            int currX = curr[0];
+            int currY = curr[1];
+
+            // Если текущая точка соответствует конечной точке, достигнут путь
+            if (currX == ox && currY == oy) {
+                // Добавляем конечную точку в путь
+                path.add(new int[]{currX, currY});
+
+                // Возвращаем путь
+                return path;
+            }
+
+            // Проходим по всем возможным направлениям движения
+            for (int[] direction : directions) {
+                int newX = currX + direction[0];
+                int newY = currY + direction[1];
+
+                // Проверяем, что новые координаты находятся в пределах лабиринта и являются проходом
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && maze[newX][newY] == 0 && !visited[newX][newY]) {
+                    // Добавляем новую точку в путь и в очередь
+                    path.add(new int[]{newX, newY});
+                    queue.offer(new int[]{newX, newY});
+
+                    // Отмечаем новую точку как посещенную
+                    visited[newX][newY] = true;
+                }
+            }
+        }
+
+        // Если достижение конечной точки невозможно, возвращаем сообщение об ошибке
+        System.out.println("Путь от начальной до конечной точки невозможен.");
+        return null;
     }
 }
