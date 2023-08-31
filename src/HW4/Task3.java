4d069fefbb265ddbbbc24a455ee2439546a3f235
Kristina <Bolshakovakristy@gmail.com>
2023-08-31 18:28:14 +0300

    HW4 add task 3

---
+++ src/HW4/Task3.java
@@ -0,0 +1,27 @@
+//Функция-редуктор:
+// Напишите функцию-редуктор (или функцию свертки), которая принимает начальное значение и список чисел,
+// а затем возвращает произведение всех чисел в списке.
+// Используйте эту функцию для вычисления произведения чисел в списке.
+
+package HW4;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        int[] array = {2, 5, 8, 2, 6, 4};
+        int num = 1;
+        System.out.println("Произведение чисел в списке = " + prod(num, array));
+    }
+
+    public static int prod(int n, int[] arr){
+        for (int j : arr) {
+            n *= j;
+        }
+        if (arr.length == 0){
+            return 0;
+        }
+        else {
+            return n;
+        }
+    }
+}
